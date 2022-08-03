2714075f1851090c48b1818dbed4b62dfaa92a2c
Artur <110206859+0FFD4H00K@users.noreply.github.com>
2022-08-03 16:33:20 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Homework.lesson5.easy;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.fullName = "Jack";
+        person.age = 35;
+        person.move();
+        person.talk();
+
+    }
+}
---
+++ Person.java
@@ -0,0 +1,18 @@
+package Homework.lesson5.easy;
+
+public class Person {
+
+    String fullName;
+    int age;
+
+    void move() {
+        System.out.println(fullName + " is moving");
+
+    }
+
+    void talk() {
+        System.out.println(fullName + " is talking");
+
+    }
+}
+
