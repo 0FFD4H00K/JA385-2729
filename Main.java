0b37c73d55089e90a33a17dad57735c5063f3aa5
Artur <110206859+0FFD4H00K@users.noreply.github.com>
2022-08-03 16:41:09 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
-package Homework.lesson5.normal;
+package Homework.lesson5.easy;
 
 public class Main {
     public static void main(String[] args) {
+        Person person = new Person();
+        person.fullName = "Jack";
+        person.age = 35;
+        person.move();
+        person.talk();
 
-        Car car = new Car();
-        Driver driver = new Driver();
-        Engine engine = new Engine();
-
-        car.start();
-        car.stop();
-        car.turnRight();
-        car.turnLeft();
-
-        car.model = "Bmw m5";
-        car.brand = "BMW";
-        car.weight = 50;
     }
-
 }
-
